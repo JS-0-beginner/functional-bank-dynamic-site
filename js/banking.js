f9053dee3f6dc69ce4cc42acdec9a2472844565a
avishekataur <avishekataur@gmail.com>
2021-08-16 12:27:32 +0600

    alright

--- js/banking.js
+++ js/banking.js
@@ -26,7 +26,7 @@ function()
   const getCurrentDepositText = currentDeposit.innerText; 
   // currentDeposit.innerText = getDepositAmountText; //result
 
-  //(3-ii)change the Display field's result to float-number
+  //(3-i)change the Display field's result to float-number
   getCurrentDeposit = parseFloat(getCurrentDepositText);
 
   //(4)adding new continuous input field's value to previous Display field's result
@@ -42,7 +42,7 @@ function()
   
 
 
-  //(3-i)Clear input field
+  //(3-ii)Clear input field
   depositAmount.value= '';
 
 }
@@ -52,5 +52,45 @@ function()
   /////////// Withdraw \\\\\\\\\\\\\
 \*------------------------------------*/
 
+//(1)Button Event Handler
+document.getElementById('withdraw-button').addEventListener('click',
+
+function()
+{
+  // console.log('withdraw button clicked'); //Test
+
+  //(2)input field's value
+  const withdrawAmount = document.getElementById('withdraw-amount');
+  const getWithdrawAmountText = withdrawAmount.value;
+  // console.log(getWithdrawAmountText); //Test
+
+  //(2-i)change the input field's value to float-number
+  const getWithdrawAmount = parseFloat(getWithdrawAmountText);
+
+  //(3)Connect (2) to Display field's result 
+  const currentWithdraw = document.getElementById('current-withdraw');
+  const getCurrentWithdrawText = currentWithdraw.innerText;
+  // console.log(getCurrentWithdrawText); //Test
+  currentWithdraw.innerText = getWithdrawAmount; //result
+
+  //(3-i)change the Display field's result to float-number
+  const getCurrentWithdraw = parseFloat(getCurrentWithdrawText);
+
+  //(4)Subtracting new continuous input field's value to previous Display field's result
+  currentWithdraw.innerText = getCurrentWithdraw + getWithdrawAmount;
+
+  //(5)Update Withdraw in Balance
+  const currentBalance = document.getElementById('current-balance');
+  const currentBalanceText = currentBalance.innerText;
+  const previousBalance = parseFloat(currentBalanceText);
+
+  currentBalance.innerText = previousBalance - getWithdrawAmount;
+
+  //(3-ii)Clear input field
+  withdrawAmount.value = '';
+
+}
+
+)
 
 
