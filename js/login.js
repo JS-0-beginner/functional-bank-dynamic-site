423bc300b9e8aeff445742df6da363c2806d6ef6
avishekataur <avishekataur@gmail.com>
2021-08-16 21:27:16 +0600

    decoration-done

---
+++ js/login.js
@@ -0,0 +1,26 @@
+/////////// Log in Page \\\\\\\\\\\\\\\\\\\
+
+document.getElementById('login-button').addEventListener('click',
+
+function()
+{
+    //For email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    console.log(userEmail);
+
+    //For password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+    console.log(userPassword);
+
+    if(userEmail == 'abc@mail.com' && userPassword == 'secret')
+    {
+        window.location.href = 'bank.html';
+    }
+    
+}
+
+)
+
+
