98b0ebeca0e046b681801a198de8c4b541afd85c
avishekataur <avishekataur@gmail.com>
2021-08-16 20:37:55 +0600

    function-kerfa

--- js/banking-function.js
+++ js/banking-function.js
@@ -1,3 +1,7 @@
+/*----------------------------------------------------------------------*\
+  //////////////////////// Function Demo \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\*----------------------------------------------------------------------*/
+
 /* 
 function doubleTheNumber(number)
 {
@@ -8,49 +12,73 @@ const result = doubleTheNumber(7);
 const result = doubleTheNumber(5); 
 */
 
-function getInputValue()
+function getInputValue(inputId)
 {
-    //(2)input field's value
-  const depositAmount = document.getElementById('deposit-amount');
-  const getDepositAmountText = depositAmount.value;
-  // console.log(getDepositAmountText); //Test
+    
+  const inputField = document.getElementById(inputId);
+  const inputAmountText = inputField.value;
+  const amount = parseFloat(inputAmountText);
+  
+  inputField.value= '';
 
-  //(2-i)change the input field's value to float-number
-  getDepositAmount = parseFloat(getDepositAmountText);
-
-  //(3-ii)Clear input field
-  depositAmount.value= '';
-
-  return depositAmount;
+  return amount;
 
 }
 
-// Event Handler
-document.getElementById('deposit-button').addEventListener('click',
-
-function()
+function updateTotalField(totalField, amount)
 {
-    depositAmount = getInputValue(); //Deposit-Button
-    
-  //(3)Connect (2) to Display field's result 
-  const currentDeposit = document.getElementById('current-deposit');
-  const getCurrentDepositText = currentDeposit.innerText; 
-  // currentDeposit.innerText = getDepositAmountText; //result
+  debugger;
+  const totalElement = document.getElementById(totalFieldId);
+  const totalText = totalElement.innerText; 
+  
+  const previousTotal = parseFloat(totalText);
 
-  //(3-i)change the Display field's result to float-number
-  getCurrentDeposit = parseFloat(getCurrentDepositText);
+  totalElement.innerText = previousTotal + amount;
+}
 
-  //(4)adding new continuous input field's value to previous Display field's result
-
-  currentDeposit.innerText = getCurrentDeposit + getDepositAmount;
-
-  //(5)Update Deposit in Balance
+function updateBalance()
+{
   const currentBalance = document.getElementById('current-balance');
   const currentBalanceText = currentBalance.innerText;
   const previousBalance = parseFloat(currentBalanceText);
 
   currentBalance.innerText = previousBalance + getDepositAmount;
   
+}
+
+
+/*-------------------------------------------*\
+  /////////////// Deposit \\\\\\\\\\\\\\\\\\\
+\*-------------------------------------------*/
+
+document.getElementById('deposit-button').addEventListener('click',
+
+function()
+
+{
+  const depositAmount = getInputValue('deposit-input');
+  if(depositAmount > 0)
+  {
+    updateTotalField('total-deposit', depositAmount);
+    updateBalance(depositAmount, true);
+  }
+  
+
+}
+
+);
+
+/*------------------------------------*\
+  /////////// Withdraw \\\\\\\\\\\\\
+\*------------------------------------*/
+
+document.getElementById('withdraw-button').addEventListener('click',
+function()
+{
+  updateTotalField('withdraw-amount', withdrawAmount)
+
+
+
 }
 
 )
